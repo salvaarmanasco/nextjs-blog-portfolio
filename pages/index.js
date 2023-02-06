40ca627a4437d6f25a6e5c7520576763151a9472
Salvaarmanasco <salvador.armanasco@gmail.com>
2023-02-06 12:15:27 -0300

    CVDownload

--- pages/index.js
+++ pages/index.js
@@ -160,8 +160,7 @@ export default function Home() {
                 </li>
                 <li>
                   <a
-                    href="../public/CV-2023-SalvadorArmanasco.pdf"
-                    download
+                    href="https://drive.google.com/drive/folders/1jmXvUtD0vd_s1Ot6AK_VieKL5sSgs_q7?usp=sharing"
                     className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   >
                     Resume
